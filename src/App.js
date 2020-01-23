9c16a752e77611a7bb106a5f04d2b732dadfbf8d
Prasad <prasadbhat4569@gmail.com>
2020-01-23 13:13:09 +0530

    TYPE2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const RadioComponent = () => {
+  const [isB2C, setB2C] = useState(false);
+  const [text, setText] = useState("");
+  const onSubmit = e => {
+    e.preventDefault();
+    const data = {
+      isB2C,
+      text
+    };
+    console.log(data);
+  };
+  const handleRadio = e => {
+    if (e.target.value === "individual") {
+      setB2C(true);
+    } else {
+      setB2C(false);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12">
+          <h2>who is your customer?</h2>
+          <hr />
+          <form onSubmit={onSubmit}>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value="individual"
+                  name="rad"
+                  onChange={e => handleRadio(e)}
+                />
+                Individual
+              </label>
+            </div>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value="company"
+                  name="rad"
+                  onChange={e => handleRadio(e)}
+                />
+                Company
+              </label>
+            </div>
+            <input
+              type="text"
+              value={text}
+              onChange={e => setText(e.target.value)}
+            ></input>
+            <br />
+            <br />
+            <button className="btn btn-primary" type="submit">
+              Save
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
+export default RadioComponent;
